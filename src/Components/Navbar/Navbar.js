1f7315f7dfbe6da5faa9214da3d18545da4b7c87
Yash Patel <patelymp007@gmail.com>
2021-03-01 11:14:11 -0800

    Final Submission

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,19 +6,19 @@ export default function Navbar() {
         <div>
             <nav className="navbar navbar-light">
                 <a className="navbar-brand">Brand</a>
-                <div class="nav-item dropdown">
-                    <a class="nav-link" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <div className="nav-item dropdown">
+                    <a className="nav-link" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <div className="profile">
                             <img src="profile.jpg" className="rounded-circle" alt="Cinque Terre" />
                             <p><div className="dropdown-toggle">Yashkumar Patel</div>Frontend Developer</p>
 
                         </div>
                     </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="#">Profile</a>
-                        <a class="dropdown-item" href="#">Account Setting</a>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a className="dropdown-item" href="#">Profile</a>
+                        <a className="dropdown-item" href="#">Account Setting</a>
                         <div className="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Logout</a>
+                            <a className="dropdown-item" href="#">Logout</a>
                     </div>
                 </div>
             </nav>
